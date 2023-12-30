0e712338c3f156dd337f9b3dec271fe00706742b
Tabassum Huda Oyshi <137924593+2021331070oyshi@users.noreply.github.com>
2023-12-30 23:34:58 +0600

    Update catchtheball.cpp

--- catchtheball.cpp
+++ catchtheball.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 //creating our game field
 const int BOARD_SIZE=10;
-const char EMPTY=' ';
-const char BALL='O';
-const char PLAYER='P';
+const char empty=' ';
+const char ball='O';
+const char player='P';
