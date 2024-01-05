80dc0f74162a3ededa68f1ed891622da24b6f52d
Tabassum Huda Oyshi <137924593+2021331070oyshi@users.noreply.github.com>
2024-01-05 22:47:55 +0600

    Create catchtheball.c

---
+++ catchtheball.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time>
+
+
+
+const char blank = ' ';
+const char ball = 'O';
+const char player = 'P';
+
+int main(){
+srand(time(0));
+
+int bsz,lev ; // board size and level
+scanf("Choose a level: \n 1. easy   2. medium   3. hard\n press a number 1/2/3: %d",&lev);
+if(lev==3) bsz=4; else if(lev==2) bsz=6; else bsz=10;
+
+
+char brd[bsz][bsz]; // board
+int prow = bsz - 1;// last row te
+ int pcol = bsz/ 2;// middle e nambe at first
+int brow = 0;// first row te ball
+ int bcol = rand() % bsz;// less than board size, array er index ball  
+
+
+ for (int i= 0; i < bsz; i++) {
+ for (int j = 0; col < BOARD_SIZE; ++col) {// column ar row same size
+board[row][col] = EMPTY; // at first empty kora
+ }
+}
